97eeb6cf00f1619a873af447aac92062321ffed2
JuanPalbo <juan.cerizola@correo.ucu.edu.uy>
2023-09-18 10:58:35 -0300

    Typos armas

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -12,9 +12,9 @@ namespace Program
             Elf elf2 = new Elf("Samantis", 10, 2, 3);
             Wizard wizard = new Wizard("Gandalf", 10, 4, 2);
             Dwarf dwarf = new Dwarf("Bruenor", 15, 2, 4);
-            Weapon bow1 = new Weapon("Bow of the Galadhrim", 0, 2);
-            Weapon bow2 = new Weapon("Ice Hammer", 0, 3);
-            elf1.Equip(bow1);
+            Weapon bow = new Weapon("Bow of the Galadhrim", 0, 2);
+            Weapon hammer = new Weapon("Ice Hammer", 0, 3);
+            elf1.Equip(bow);
             Spellbook spellbook = new Spellbook("uno", 0, 0, new List<Spell>());
             Spell spell = new Spell("Firebolt", 2);
             spellbook.AddSpell(spell);
