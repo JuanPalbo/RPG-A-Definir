31a7772af4cfadd9c0771c89da7932ca5b64a81c
JuanPalbo <134970060+JuanPalbo@users.noreply.github.com>
2023-09-18 10:51:42 -0300

    Arreglo Program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -14,13 +14,13 @@ namespace Program
             Dwarf dwarf = new Dwarf("Bruenor", 15, 2, 4);
             Weapon bow1 = new Weapon("Bow of the Galadhrim", 0, 2);
             Weapon bow2 = new Weapon("Ice Hammer", 0, 3);
-            elf.Equip(bow);
+            elf1.Equip(bow1);
             Spellbook spellbook = new Spellbook("uno", 0, 0, new List<Spell>());
             Spell spell = new Spell("Firebolt", 2);
             spellbook.AddSpell(spell);
             wizard.Equip(spellbook);
-            wizard.Attack(elf);
-            dwarf.Heal(elf);
+            wizard.Attack(elf1);
+            dwarf.Heal(elf1);
             
         }
     }
