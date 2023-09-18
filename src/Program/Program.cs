e6b50e4376099734b0869a6c523759b322b5d73d
Cristof <cristof.kunze.correo.ucu.edu.uy>
2023-09-18 10:49:09 -0300

    personajes y objetos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,10 +8,12 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Elf elf = new Elf("Legolas", 10, 3, 1);
+            Elf elf1 = new Elf("Legolas", 10, 3, 1);
+            Elf elf2 = new Elf("Samantis", 10, 2, 3);
             Wizard wizard = new Wizard("Gandalf", 10, 4, 2);
             Dwarf dwarf = new Dwarf("Bruenor", 15, 2, 4);
-            Weapon bow = new Weapon("Bow of the Galadhrim", 0, 2);
+            Weapon bow1 = new Weapon("Bow of the Galadhrim", 0, 2);
+            Weapon bow2 = new Weapon("Ice Hammer", 0, 3);
             elf.Equip(bow);
             Spellbook spellbook = new Spellbook("uno", 0, 0, new List<Spell>());
             Spell spell = new Spell("Firebolt", 2);
