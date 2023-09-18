2bacf204a840782720d96c5eaff3b984e04dc53c
JuanPalbo <juan.cerizola@correo.ucu.edu.uy>
2023-09-18 10:25:27 -0300

    Pelea entre gandalf y legolas en program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,9 +8,18 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            //create an elf and a wizard
-            Elf elf = new Elf("Legolas", 100, 10, 10);
-            Wizard wizard = new Wizard("Gandalf", 100, 10, 10);
+            Elf elf = new Elf("Legolas", 10, 3, 1);
+            Wizard wizard = new Wizard("Gandalf", 10, 4, 2);
+            Dwarf dwarf = new Dwarf("Bruenor", 15, 2, 4);
+            Weapon bow = new Weapon("Bow of the Galadhrim", 0, 2);
+            elf.Equip(bow);
+            Spellbook spellbook = new Spellbook("uno", 0, 0, new List<Spell>());
+            Spell spell = new Spell("Firebolt", 2);
+            spellbook.AddSpell(spell);
+            wizard.Equip(spellbook);
+            wizard.Attack(elf);
+            dwarf.Heal(elf);
+            
         }
     }
 }
