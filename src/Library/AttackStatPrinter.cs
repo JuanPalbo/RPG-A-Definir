736d5d6d18a0d0e3bf594cf39dfdbcda9fd43f5a
JuanPalbo <juan.cerizola@correo.ucu.edu.uy>
2023-09-18 11:11:26 -0300

    fix impresion daños

--- src/Library/AttackStatPrinter.cs
+++ src/Library/AttackStatPrinter.cs
@@ -4,6 +4,25 @@ public class AttackStatPrinter
 {
     public static void PrintAttackStat(ICharacter character)
     {
-        Console.WriteLine($"{character.Name} has {character.StatAttack} attack.");
+        if (character is Wizard)
+        {
+            int spellDmg = 0;
+            foreach(IItem i in character.Inventory)
+            {
+                if(i is Spellbook)
+                {
+                    foreach(Spell j in Spellbook.Spells)
+                    {
+                        spellDmg += j.AttackValue;
+                    }
+                }
+            }
+            Console.WriteLine($"{character.Name} has {character.StatAttack+spellDmg} attack.");
+        }
+        else
+        {
+            Console.WriteLine($"{character.Name} has {character.StatAttack} attack.");
+        }
+        
     }
 }
